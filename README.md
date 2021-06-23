f3bdaca30102793575cd7f1c74726eb5d39f1c95
Arve Moen <arve.moen'me.com>
2021-06-24 01:00:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Home Automation
+Home automation repository for container based Home Assistant and Mosquitto
+
+## My home automation landscape
+![](img/SmartHome.bmp)
+
+## Home Assistant
+
+### Themes
+Themes downloaded from [maartenpaauw/home-assistant-community-themes](https://github.com/maartenpaauw/home-assistant-community-themes)
+
+### Custom Components
+- [HACS](https://hacs.xyz/)
+- [thomasloven/lovelace-card-mod](https://github.com/thomasloven/lovelace-card-mod)
+- [kalkih/mini-graph-card](https://github.com/kalkih/mini-graph-card)
+- [marksie1988/atomic-calendar-revive](https://github.com/marksie1988/atomic-calendar-revive)
+
---
+++ home/homeassistant/config/file_dump/run_speedtest.cmd
@@ -0,0 +1,30 @@
+REM ************************************************************************************************
+REM Runs speedtest cli and saves the results to a json file that will be retrieved by Home Assistant
+REM which is running in a docker container. The folder must be on a shared docker volume and be
+REM defined in the "allowlist_external_dirs:" section of the configuration.yaml file.
+REM 
+REM To be run run by task scheduler at regular intervals
+REM ************************************************************************************************
+
+@echo off
+
+cd %~dp0
+
+REM File and folder configs
+set SPEEDTEST_FOLDER=.
+set SPEEDTEST_FILENAME=speedtest
+set SPEEDTEST_FORMAT=json
+
+REM Run speedtest against Telia Norge AS (server id 31861). Save result on file
+REM See https://c.speedtest.net/speedtest-servers-static.php for more server IDs
+speedtest.exe --server-id=31861 --format=%SPEEDTEST_FORMAT% > %SPEEDTEST_FOLDER%\%SPEEDTEST_FILENAME%_real.%SPEEDTEST_FORMAT%
+
+REM Remove ending CRLF in file
+for /F usebackq^ delims^=^ eol^= %%L in ("%SPEEDTEST_FOLDER%\%SPEEDTEST_FILENAME%_real.%SPEEDTEST_FORMAT%") do (
+    < nul set /P ="%%L" > %SPEEDTEST_FOLDER%\%SPEEDTEST_FILENAME%.%SPEEDTEST_FORMAT% 
+) 
+
+REM Remove unnecessary files
+del /q %SPEEDTEST_FOLDER%\%SPEEDTEST_FILENAME%_real.%SPEEDTEST_FORMAT% 2> nul
+
+EXIT
